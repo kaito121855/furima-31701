606984d6fea5d0dd88a283c4892dcd6a2b1bf9c6
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-11-14 12:07:52 +0900

    バリデーション設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,4 +9,16 @@ class Product < ApplicationRecord
   belongs_to :area
   belongs_to :day
   
+  with_options presence: true do
+    validates :name
+    validates :explanation
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :burden_id
+    validates :area_id
+    validates :day_id
+  end
+  validates :price, presence: true, format: { /\A[0-9]+\z/ }
 end
