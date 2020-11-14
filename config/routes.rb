7f1162d2903f9ef0aa7406ca1e5ec561f27a693c
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-11-14 18:37:35 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'items/index'
   root to: "items#index"
-  resources :itees, only: [:index, :new, :create]
+  resources :products, only: [:index, :new, :create]
 end
