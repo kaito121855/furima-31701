0ef71c1d2bb04b7374b6cc8ddec3472bd6fbfb84
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-11-14 21:25:12 +0900

    アクション設定（create）とストロングパラメーター設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,23 @@ class ProductsController < ApplicationController
 
   def index
   end
+
   def new
-    @item = Product.new
+    @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+end
+
+private
+
+def product_params
+  params.require(:product).permit(:image, :name, :explanation, :category_id, :status_id, :burden_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
 end
