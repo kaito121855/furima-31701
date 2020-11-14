18f90bfdbff9e541cab47ec563c64b7321d0f7e5
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-11-14 11:37:20 +0900

    ActionHashに関するアソシエーション

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,12 @@
 class Product < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :area
+  belongs_to :day
   
 end
