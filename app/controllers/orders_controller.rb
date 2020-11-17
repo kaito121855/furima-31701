b89dcc8e30a8482831320df0a61b9f3d5ad56c00
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-11-17 19:38:56 +0900

    ordersコントローラーの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,23 @@
 class OrdersController < ApplicationController
 
   def index
+    @product = Product.find(params[:id])
     @order_form = OrderForm.new
   end
 
   def create
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      @order_form.save
+      redirect_to "/products/#{@order_form.product.id}"
+    else
+      render action: :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_form).permit(:postal_code, :prefecture_id ,:municipality, :address, :building, :telephone, :purchase).merge(token: params[:token], user_id: current_user.id, product_id: params[:product_id])
   end
 
 end
