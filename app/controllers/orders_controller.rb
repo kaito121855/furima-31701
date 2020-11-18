0ff05a1b4ce43bd534939bbbc5fb0f741d892be1
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-11-18 17:36:19 +0900

    コントローラーの設定（Payjp）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,22 @@
 class OrdersController < ApplicationController
+  before_action :set_product, only: [:index, :create]
 
   def index
-    @product = Product.find(params[:product_id])
     @order_form = OrderForm.new
   end
 
   def create
+    # binding.pry
     @order_form = OrderForm.new(order_params)
     if @order_form.valid?
+      Payjp.api_key = "sk_test_ff68a3056119ddc5f9d5a815"
+      Payjp::Charge.create(
+        amount: @product.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
       @order_form.save
-      redirect_to "/products/#{@order_form.product.id}"
+      redirect_to root_path
     else
       render action: :index
     end
@@ -17,7 +24,11 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order_form).permit(:postal_code, :prefecture_id ,:municipality, :address, :building, :telephone, :purchase).merge(token: params[:token], user_id: current_user.id, product_id: params[:product_id])
+    params.require(:order_form).permit(:postal_code, :prefecture_id, :municipality, :address, :building, :telephone).merge(token: params[:token], user_id: current_user.id, product_id: params[:product_id])
+  end
+
+  def set_product
+    @product = Product.find(params[:product_id])
   end
 
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -8,12 +8,11 @@ class OrderForm
     validates :municipality
     validates :address
     validates :telephone, format: { with: /\A[0-9]+\z/ }
-    validates :purchase
   end
   validates :prefecture_id, numericality: { other_than: 1 }
 
   def save
-    order = Order.create(token: token, pruduct_id: product.id, user_id: user.id)
-    Residence.create(postal_code: postal_code, prefecture_id: prefecture_id ,municipality: municipality, address: address, building: building, telephone: telephone, purchase: purchase, order_id: order.id)
+    order = Order.create(product_id: product_id, user_id: user_id)
+    Residence.create(postal_code: postal_code, prefecture_id: prefecture_id ,municipality: municipality, address: address, building: building, telephone: telephone, order_id: order.id)
   end
 end
