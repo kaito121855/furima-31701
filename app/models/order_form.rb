042bd03adc6a548306999f7843f48001c4825ddc
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-11-19 12:03:23 +0900

    電話番号のバリデーションの追加

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,13 +1,13 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :token, :postal_code, :prefecture_id ,:municipality, :address, :building, :telephone, :purchase, :user_id, :product_id
+  attr_accessor :token, :postal_code, :prefecture_id ,:municipality, :address, :building, :telephone, :user_id, :product_id
 
   with_options presence: true do
     validates :token
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :municipality
     validates :address
-    validates :telephone, format: { with: /\A[0-9]+\z/ }
+    validates :telephone, format: { with: /\A[0-9]+\z/ }, length: { maximum: 11 }
   end
   validates :prefecture_id, numericality: { other_than: 1 }
 
